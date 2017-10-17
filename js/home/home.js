935ede3bbb9e85088cf7bbc62c04494780b78211
allen-parslow <allen1jobs@gmail.com>
2017-10-16 22:00:30 -0500

    [feature] added home components

---
+++ js/home/home.js
@@ -0,0 +1,24 @@
+import React from "react";
+import i18n from "../i18n/i18n";
+
+import Navbar from "../navbar/navbar";
+import Footer from "../footer/footer";
+
+import "./home.scss";
+
+export default class Home extends React.Component {
+    render() {
+        return (
+            <div>
+                <Navbar/>
+                    <div className="welcome container-fluid">
+                        <div className="container xd-container">
+                            <h1>{i18n.t("TEXT__WELCOME_GREETING")}</h1>
+                            <img className="img-responsive" src="/assets/images/pets.png"/>
+                        </div>
+                    </div>
+                <Footer/>
+            </div>
+        );
+    }
+}
---
+++ js/navbar/navbar.js
@@ -0,0 +1,45 @@
+import React from "react";
+import i18n from "../i18n/i18n";
+
+require("./navbar.scss");
+
+class NavBarItem extends React.Component {
+    render() {
+        let title = i18n.t(this.props.name);
+        return (
+            <li className={this.props.active ? "active" : ""}>
+                <a href={this.props.href} title={this.title}>
+                    <span className={"glyphicon glyphicon-" + this.props.icon} aria-hidden="true"></span>
+                    <span>{title}</span>
+                </a>
+            </li>
+    );}
+}
+
+export default class Navbar extends React.Component {
+    render() {
+        return (
+            <nav className="navbar navbar-default" role="navigation">
+                <div className="container">
+                    <div className="navbar-header">
+                        <a className="navbar-brand" href="/"><span/></a>
+                        <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#main-navbar">
+                            <span className="sr-only"><os-p>Toggle navigation</os-p></span>
+                            <span className="icon-bar"></span>
+                            <span className="icon-bar"></span>
+                            <span className="icon-bar"></span>
+                        </button>
+                    </div>
+                    <div className="navbar-collapse collapse" id="main-navbar">
+                        <ul className="nav navbar-nav navbar-right">
+                            <NavBarItem active={true} href="/" name="NAV__HOME" icon="home" />
+                            <NavBarItem href="/owners/find" name="NAV__OWNERS" icon="search" />
+                            <NavBarItem name="NAV__VETERINARIANS" icon="th-list" />
+                            <NavBarItem name="NAV__ERROR" icon="warning-sign" />
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        );
+    }
+}
