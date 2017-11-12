0c25330220e247cc1546443d823bc71762600533
Allen Parslow <allen1jobs@gmail.com>
2017-11-12 15:06:02 -0600

    [feature] migrating to redux

--- packages/petclinic/src/entry.js
+++ packages/petclinic/src/entry.js
@@ -6,20 +6,44 @@ import {
   Link
 } from "react-router-dom";
 
+const counter = (state = { counter: 0 }, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return Object.assign({}, { counter: state.counter + 1 });
+    case "DECREMENT":
+    return Object.assign({}, { counter: state.counter - 1 });
+    default:
+      return state;
+  }
+};
+let store = createStore(counter,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+store.subscribe(() =>
+console.log(JSON.stringify(store.getState()))
+);
+
+
+store.dispatch({ type: "INCREMENT" });
+store.dispatch({ type: "INCREMENT" });
+
+
 import Home from "./home/home";
 import Vets from "./vets/vets";
 import Owners from "./owners/owners";
 
 require("./entry.scss");
 
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+
 const App = () => (
-  <Router>
-    <div>
-      <Route exact path="/" component={Home}/>
-      <Route path="/vets" component={Vets}/>
-      <Route path="/owners" component={Owners}/>
-    </div>
-  </Router>
+  <Provider store={store}>
+    <Owners/>
+  </Provider>
 );
+
 
 ReactDOM.render(<App/>, document.getElementById("app"));
--- packages/petclinic/src/owners/owners.js
+++ packages/petclinic/src/owners/owners.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { connect } from "react-redux";
 import i18n from "i18n-lite";
 
 import Navbar from "../navbar/navbar";
@@ -6,7 +7,20 @@ import Footer from "../footer/footer";
 
 import "./owners.scss";
 
-const Owners = () => {
+const mapStateToProps = (state, ownProps) => {
+    console.log("mapStateToProps=" + JSON.stringify(state) + " -|- " + JSON.stringify(ownProps) );
+    return {
+        counter: state.counter
+    };
+  };
+
+const CounterAmt = connect(mapStateToProps)(({counter}) => {
+    console.log("CounterAmt=" + JSON.stringify(counter));
+    return <div>Counter: {counter}</div>;
+});
+
+const Owners = ({ dispatch }) => {
+    dispatch({ type: "INCREMENT" });
     return <div>
         <Navbar selected="owners" />
         <div className="owners container xd-container">
@@ -16,6 +30,7 @@ const Owners = () => {
                     <div className="control-group" id="lastName">
                         <label className="col-sm-2 control-label">{i18n.t("TEXT__LAST_NAME")}</label>
                         <div className="col-sm-10">
+                            <CounterAmt extended={true}/>
                             <input className="form-control" size="30"
                                 maxLength="80" name="lastName" /> <span className="help-inline"></span>
                         </div>
@@ -23,7 +38,7 @@ const Owners = () => {
                 </div>
                 <div className="form-group">
                     <div className="col-sm-offset-2 col-sm-10">
-                        <button className="btn btn-default">Find Owner</button>
+                        <button className="btn btn-default" onClick={()=>{console.log("clicked");dispatch({ type: "INCREMENT" });}}>Find Owner</button>
                     </div>
                 </div>
             </div>
@@ -32,5 +47,5 @@ const Owners = () => {
     </div>;
 };
 
-export default Owners;
+export default connect()(Owners);
 
